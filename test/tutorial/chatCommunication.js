28bb3ad07a53bb08d147e3ad36d78b6c767a489b
alexshi0000 <alexshi0000@gmail.com>
2018-08-25 19:46:20 -0400

    ahead

--- test/tutorial/chatCommunication.js
+++ test/tutorial/chatCommunication.js
@@ -17,12 +17,10 @@ app.get("/socket", function(req, res){
 //turn on and connect/check for connecetion to localhost
 io.on('connection', function(socket){
     console.log("connected");
-    
     //check for dicconnection
     socket.on('disconnect', function(){
         console.log("disconnect");
     });
-    
     //check for chat message and send the user input msg to everyone
     socket.on("chat message", function(msg){
         //send to eveyone, can be specified to who
