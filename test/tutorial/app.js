824b36505799d3235d7a619038abdff4a5c4845e
alexshi0000 <alexshi0000@gmail.com>
2018-08-25 20:27:20 -0400

    chat app minor changes

--- test/tutorial/app.js
+++ test/tutorial/app.js
@@ -3,7 +3,7 @@ const logger = new Logger()
 
 //register a listener
 logger.on('messageLogged', (arg) => {
-	console.log('listener called', arg)
+  console.log('listener called', arg)
 })
 
 logger.log('here is a message')
