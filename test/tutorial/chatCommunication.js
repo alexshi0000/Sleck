54e93aa884c3598c8fb4373a2e036410ffa614fc
Yu An Shi <alexshi0000@gmail.com>
2018-09-12 23:07:47 -0400

    Update chatCommunication.js

--- test/tutorial/chatCommunication.js
+++ test/tutorial/chatCommunication.js
@@ -14,16 +14,12 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/Slecktest.html");
 });
 
-//turn on and connect/check for connecetion to localhost
 io.on('connection', function(socket){
     console.log("connected");
-    //check for dicconnection
     socket.on('disconnect', function(){
         console.log("disconnect");
     });
-    //check for chat message and send the user input msg to everyone
     socket.on("chat message", function(msg){
-        //send to eveyone, can be specified to who
         io.emit("chat message",msg);
     });
 });
