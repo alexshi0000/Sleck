69b820ed2143ba70baaa3a34dceae2abbe7dd3e6
alexshi0000 <alexshi0000@gmail.com>
2018-12-22 12:40:16 -0500

    .

--- app/app.js
+++ app/app.js
@@ -9,22 +9,22 @@ app.use(express.static('public'));
 
 //index
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/public/html/index.html')
 })
+
+//html
+app.get('/public/html/error.html')
 
 //js
 app.get('/public/js/socket.js', (req, res) => {
   res.sendFile(__dirname + '/public/js/socket.js')
 })
-
 app.get('/public/js/textfield.js', (req, res) => {
   res.sendFile(__dirname + '/public/js/textfield.js')
 })
-
 app.get('/public/js/messages.js', (req, res) => {
   res.sendFile(__dirname + '/public/js/messages.js')
 })
-
 app.get('/public/js/sidebar.js', (req, res) => {
   res.sendFile(__dirname + '/public/js/sidebar.js')
 })
@@ -33,21 +33,17 @@ app.get('/public/js/sidebar.js', (req, res) => {
 app.get('/public/css/index.css', (req, res) => {
   res.sendFile(__dirname + '/public/css/index.css')
 })
-
 app.get('/public/css/messages.css', (req, res) => {
   res.sendFile(__dirname + '/public/css/messages.css')
 })
-
 app.get('/public/css/sidebar.css', (req, res) => {
   res.sendFile(__dirname + '/public/css/sidebar.css')
 })
-
 app.get('/public/css/textfield.css', (req, res) => {
   res.sendFile(__dirname + '/public/css/textfield.css')
 })
-
 app.get('*', (req, res) => {
-  res.send('<h1>404</h1>', 404);
+  res.send('<h1>404</h1>', 404); //send a 404
 });
 
 // ====================== Event Handling =======================================
