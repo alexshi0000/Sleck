32c0f508cf92a5820b42f6a3ca37877788134887
alexshi0000 <alexshi0000@gmail.com>
2018-12-24 15:24:03 -0500

    fixed error client side connection socketio

--- app/public/js/error.js
+++ app/public/js/error.js
@@ -1,6 +1,6 @@
-var socket = io()
-
-(document).ready(() => {
+$(document).ready(() => {
+  var socket = io()
+  socket.emit('set error message')
   socket.on('set error message', (msg) => {
     console.log(msg + 'being appended to error page')
     $('#errors').append('<h3>' + msg + '</h3>')
